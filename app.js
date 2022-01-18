2649ba667ecdb0544e92f2a9df0d001f7817a13b
manjeete <manjeetkr2017@gmail.com>
2022-01-18 14:32:27 +0530

    register route

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ require('dotenv').config()
 
 const sendotp = require('./utils/sentOtp');
 
+//models
+const Otp = require("./models/otp");
+const User = require("./models/user");
+
 mongoose
     .connect(process.env.DATABASE, {
         useNewUrlParser: true,
@@ -26,11 +30,40 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.get("/",(req,res) =>{
-    sendotp("+919648666718")
+    // sendotp("+919648666718")
     res.status(200).json({
         status:true,
         msg:"Working..."
     })
+})
+
+//send otp for registration
+app.post("/register",async(req,res) =>{
+    try{
+        const phone = req.body.phone;
+        let userCheck = await User.findOne({phone:phone})
+        if(userCheck){
+            return res.status(400).json({
+                status:false,
+                msg:"User with this phone already exists."
+            })
+        }
+        let otp = await sendotp(phone);
+        if(!otp.status){
+            return res.status(400).json({
+                status:false,
+                msg:otp.msg
+            })
+        }
+        let checkOtpObj = await Otp.findOneAndDelete({phone:phone});
+        let createOtpRecord = new Otp({phone:phone,otp:otp.otp});
+        let saveOtp = await createOtpRecord.save();
+
+        res.json(otp)
+        
+    }catch(err){
+        console.log(err)
+    }
 })
 
 app.listen(PORT,() =>{
