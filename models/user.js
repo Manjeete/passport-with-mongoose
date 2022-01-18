4320a571fe3084a3334fcebca4c62f03887b0348
manjeete <manjeetkr2017@gmail.com>
2022-01-18 14:05:53 +0530

    otp and user model added:

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userType:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    }
+
+},
+    {timestamps:true}
+);
+
+module.exports = mongoose.model("User",userSchema,"User");
