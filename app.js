769d6d251488a3ec94856e5ccc2cf4c063398ccb
manjeete <manjeetkr2017@gmail.com>
2022-01-18 13:55:34 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+var path = require("path");
+const bodyParser = require("body-parser");
+var morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database Connected Successfully"))
+    .catch((err) => console.log(err));
+
+
+const app = express();
+
+const PORT = 8080
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get("/",(req,res) =>{
+    res.status(200).json({
+        status:true,
+        msg:"Working..."
+    })
+})
+
+app.listen(PORT,() =>{
+    console.log(`Server is running at port ${PORT}`)
+})
